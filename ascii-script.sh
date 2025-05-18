8163ed0d259724862734c0037d0d6a7c1ed7efe2
Hanyinson Alex <calex1731@gmail.com>
2025-05-17 22:49:49 -0400

    multi jobs

--- ascii-script.sh
+++ ascii-script.sh
@@ -4,3 +4,18 @@ cowsay -f dragon "Run for cover, I am a DRAGON....RAWR" >> dragon.txt
 greo -i "dragon" dragon.txt
 cat dragon.txt
 ls -ltra
+
+  #- name: Install Cowsay Program
+      #  run: sudo apt-get install cowsay -y
+
+      #- name: Execute Cowsay CMD
+      #  run: cowsay -f dragon "Run for cover, I am a DRAGON....RAWR" >> dragon.txt
+      
+      #- name: Test File Exists
+      #  run: grep -i "dragon" dragon.txt
+
+      #- name: Read File
+      #  run: cat dragon.txt
+
+      #- name: Checkout Repo Files
+      #  run: ls -ltra
